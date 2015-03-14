cba079fe27eb238b4561168d89730302ebc32308
allenwhitedev <allenwhitedev@gmail.com>
2015-03-14 06:11:23 -0400

    Add judge and current round helpers

--- client/gameLobby.js
+++ client/gameLobby.js
@@ -10,13 +10,13 @@ Template.gameLobby.helpers
 ({
 	'player': function()
 	{
-		console.log("here: " + GamesList.findOne({players: Meteor.userId()}).players)
+		//console.log("here: " + GamesList.findOne({players: Meteor.userId()}).players)
 		var playerIds = GamesList.findOne({players: Meteor.userId()}).players
 		//return GamesList.findOne({players: Meteor.userId()}).players
 		players = []
 		_.each(playerIds, function(id){
 			players.push(Meteor.users.findOne({_id: id}))
-			console.log("test here: " + Meteor.users.find({_id: id}))
+			//console.log("test here: " + Meteor.users.find({_id: id}))
 		})
 		return players
 	},
@@ -25,5 +25,14 @@ Template.gameLobby.helpers
 		extraUrlArray = ["http://gifgifs.com/animations/sports/other-exercises/Yoga.gif"]
 		var urlArray = ["http://gifgifs.com/animations/sports/soccer/Bouncing_ball.gif", "http://gifgifs.com/animations/sports/football/Tackle.gif", "http://gifgifs.com/animations/sports/soccer/goal.gif", "http://gifgifs.com/animations/sports/bicycling/biker.gif", "http://gifgifs.com/animations/sports/baseball/3D_bat.gif"]
 		return urlArray
+	},
+	'isJudge': function()
+	{
+		if(this.isJudge == true)
+			return true
+	},
+	'currRound': function()
+	{
+		return GamesList.findOne({players: Meteor.userId()}).round
 	}
 })
