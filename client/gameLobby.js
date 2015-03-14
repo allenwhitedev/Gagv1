898f1ba349712c747643e33c2bb84abaa3c8abb8
allenwhitedev <allenwhitedev@gmail.com>
2015-03-14 05:50:06 -0400

    Add hand to Game Lobby

--- client/gameLobby.js
+++ client/gameLobby.js
@@ -10,46 +10,20 @@ Template.gameLobby.helpers
 ({
 	'player': function()
 	{
-		// var gameUserRel = GameUserRelsList.findOne({userId: Meteor.userId()})
-		// var currGameId = gameUserRel.gameId
-		// var currGame = GamesList.findOne({_id: currGameId})
-		// playersIdsArray = []
-		// playersArray = []
-		// for (i = 0; i < currGame.players.length; i++)
-		// {
-		// 	playerIdsArray.push(currGame.players[i])
-		// }
-		// return playerIdsArray
-		// for (i = 0; i < playersArray.length; i++)
-		// {
-		// 	playersArray.push(Meteor.users.find({_id: playerIdsArray[i]}))
-		// }
-		// //return Meteor.users.find({_id: {$in: playersArray}})
-		// //return playersArray
-		// return playersArray
-		//playerIds = []
-		//players = []
-		//return GamesList.findOne({players: Meteor.userId()}).players
-		//var currUserId = Meteor.userId();
-		//console.log(currUserId);
 		console.log("here: " + GamesList.findOne({players: Meteor.userId()}).players)
 		var playerIds = GamesList.findOne({players: Meteor.userId()}).players
 		//return GamesList.findOne({players: Meteor.userId()}).players
 		players = []
-
 		_.each(playerIds, function(id){
 			players.push(Meteor.users.findOne({_id: id}))
 			console.log("test here: " + Meteor.users.find({_id: id}))
 		})
-
 		return players
-
-		//return GamesList.find({players: {$elemMatch: Meteor.userId()}})
-		//console.log
-		//playerIds = GamesList.findOne({players: Meteor.userId()}).players
-		
-		//return Meteor.users.find({_id: {$in: playerIds}})
-		//for (i = 0; i < GamesList.find({players: Meteor.userId()}).players.length; i++)
-		//	playerIds.push(GamesList.find({players: Meteor.userId()}).players[i])
+	},
+	'card': function()
+	{
+		extraUrlArray = ["http://gifgifs.com/animations/sports/other-exercises/Yoga.gif"]
+		var urlArray = ["http://gifgifs.com/animations/sports/soccer/Bouncing_ball.gif", "http://gifgifs.com/animations/sports/football/Tackle.gif", "http://gifgifs.com/animations/sports/soccer/goal.gif", "http://gifgifs.com/animations/sports/bicycling/biker.gif", "http://gifgifs.com/animations/sports/baseball/3D_bat.gif"]
+		return urlArray
 	}
 })
